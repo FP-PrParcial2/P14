916a1852ed6536e7891fa6b55199f7e3d46cc915
karen <karen@KarenZ>
2016-10-20 03:18:57 -0500

    Proyecto

---
+++ src/ppproblema014/PPProblema014.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppproblema014;
+import java.util.*;
+/**
+ *
+ * @author karen
+ */
+public class PPProblema014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* int[][] arr = new int [8][10];
+        System.out.println(arr.length);
+        for(int i=7; i<=1; i++){ 
+            for(int j=7; j<=0; j++)
+               System.out.println(arr);  
+                
+        }*/
+    }
+    
+}
